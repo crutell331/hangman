eeb544b2eab7c8281c4ffaf70bae3df85806b604
github username <github email address>
2020-11-08 10:36:54 -0800

    small style changes

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -3,5 +3,15 @@ import { Canvas } from '../components/Canvas';
 
 export const Game = () => {
     const [wrongAnswerCount, incrementWrongAnswer] = useState(0);
-    return <Canvas wrongAnswerCount={wrongAnswerCount} />;
+    const [wordSlug, updateSlug] = useState(null);
+    const [guessedLetter, updateGuess] = useState("");
+    useEffect(() => {
+        // Did Mount
+        // on did mount will fetch the length of a word  and a slug to identify that word in the API
+
+        // Event Handling
+        // on keypress should send fetch request to backend with target value and slug
+        // API should respond with either an empty array or an array with the letter's index
+    });
+    return <span className="game"><Canvas wrongAnswerCount={wrongAnswerCount} /></span>;
 };
